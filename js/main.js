f993aaf1d982333c97d1d876a253a27bb4d87901
web-sarang <77715656+web-sarang@users.noreply.github.com>
2021-11-05 16:59:45 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+
+// Burger-menu
+
+const burgerMenu = document.querySelector('.burger__menu-button');
+const bodyMenu = document.querySelector('.menu');
+
+if (burgerMenu) {
+  burgerMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        burgerMenu.classList.toggle('button--open');
+        bodyMenu.classList.toggle('menu--open');
+    });
+}
